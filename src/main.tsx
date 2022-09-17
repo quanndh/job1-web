18c39b7c6e07467edda69632a61bcd49907a8a6d
nghiadc <daonghia1308@gmail.com>
2022-09-17 14:46:14 +0700

    feat: set up react-query and axios

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,16 @@ import { ColorModeScript } from "@chakra-ui/react";
 import theme from "./theme";
 import { Web3ReactProvider } from "@web3-react/core";
 import BlockchainUtils from "../src/uilts/Blockchain";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <>
     <Web3ReactProvider getLibrary={BlockchainUtils.getLibrary}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </Web3ReactProvider>
     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
   </>
